7b7dc0f372f8cc02c6f1cc0078bbeec3f838eae4
RaymondAllie <73182219+RaymondAllie@users.noreply.github.com>
2021-04-04 04:42:34 -0400

    Update hi.php

--- hi.php
+++ hi.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
-  
-<head><link rel = "stylesheet" href = "info.css"</head>
+ 
 <title>Sign In</title>
-<link rel="shortcit icon" href="https://lh3.googleusercontent.com/tyD-CVYo8pEtgUkXxWvwLyv3udZAf09AdsM0c9N7chpmuoLCVB9p57iXU7DQRHDwy3Kjb48=s85"/>
-<h1 class = "heading">Sign Up for Isolate<h1>
+<link rel="shortcut icon" href="https://lh3.googleusercontent.com/tyD-CVYo8pEtgUkXxWvwLyv3udZAf09AdsM0c9N7chpmuoLCVB9p57iXU7DQRHDwy3Kjb48=s85"/>
 
-<div class="box">
+
+
 <form  method = 'post'>
     <br>
     <input class="sub_box" type="text" name= "username" placeholder = "Username">
@@ -19,7 +18,7 @@
   <br>
     <input id = "color" type="submit" value="Sumbit">
 </form>
-</div>
+
 <p><?php echo $_POST["username"]></p>
 
 </html>
